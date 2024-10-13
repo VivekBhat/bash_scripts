ae6868fedc1c0b7fed7c7471cd8726e1ab9c4fba
Vivek Bhat <bhatvivek93@gmail.com>
2024-10-13 13:36:33 -0400

    Refactored code to have a single installer in Unix

--- Unix/OperatingSystems/WSL/installer.sh
+++ Unix/OperatingSystems/WSL/installer.sh
@@ -6,7 +6,7 @@ SCRIPTS_FOLDER=$(dirname "$WSL_SCRIPT_DIR")
 source $SCRIPTS_FOLDER/Linux/bash_lib/common.sh
 
 # Add ZSH Profile line
-add_profile_line ~/.zshrc $WSL_SCRIPT_DIR/zsh_profile
+# add_profile_line ~/.zshrc $WSL_SCRIPT_DIR/zsh_profile
 
 # Add Bash Profile line
 add_profile_line ~/.bashrc $WSL_SCRIPT_DIR/bash_profile
