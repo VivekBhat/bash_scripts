f2ef9671b16665f03deedb103926f48fca717ecf
Vivek Bhat <vivek.bhat@hughes.com>
2024-10-14 11:25:09 -0400

    Fixed OS dir number bug

--- Unix/installer.sh
+++ Unix/installer.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
+UNIX_SCRIPT_DIR=$(dirname "$0")
 # Define the directory containing the operating systems
-os_dir="$(dirname "$(realpath "$0")")/OperatingSystems"
+OS_DIR="$UNIX_SCRIPT_DIR/OperatingSystems"
+
+source $OS_DIR/Linux/bash_lib/common.sh
 
 # Function to display available OSs
-list_os() {
+function list_os() {
     echo "Available Operating Systems:"
-    
+
     # Check if the directory exists
-    if [[ -d "$os_dir" ]]; then
+    if [[ -d "$OS_DIR" ]]; then
         local os_count=0
-        for dir in "$os_dir"/*; do
+        for dir in "$OS_DIR"/*; do
             if [[ -d "$dir" ]]; then
-                echo " [$os_count] $(basename "$dir")"  # Print directory name
+                echo " [$os_count] $(basename "$dir")" # Print directory name
                 os_count=$((os_count + 1))
             fi
         done
-        
+
         if [[ $os_count -eq 0 ]]; then
             echo "No operating systems found."
             exit 1
         fi
     else
-        echo "Directory $os_dir does not exist."
+        echo "Directory $OS_DIR does not exist."
         exit 1
     fi
 }
 
 # Function to run the installer script in the selected directory
-run_installer() {
+function run_installer() {
     local choice=$1
-    local selected_dir="$(find "$os_dir" -mindepth 1 -maxdepth 1 -type d | sed -n "$((choice + 1))p")"
-    
+    local selected_dir="$(find "$OS_DIR" -mindepth 1 -maxdepth 1 -type d | sort | sed -n "$((choice + 1))p")"
     if [[ -f "$selected_dir/installer.sh" ]]; then
         echo "Setting up $(basename "$selected_dir")..."
         bash "$selected_dir/installer.sh"
@@ -44,7 +46,7 @@ run_installer() {
 list_os
 
 # Get the total number of directories
-total_dirs=$(find "$os_dir" -mindepth 1 -maxdepth 1 -type d | wc -l)
+total_dirs=$(find "$OS_DIR" -mindepth 1 -maxdepth 1 -type d | wc -l)
 
 # Prompt for user input
 read -p "Select an operating system by entering the corresponding number: " user_input
