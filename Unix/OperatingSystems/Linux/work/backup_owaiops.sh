6469541bc6f200bf240c939640726e2dc2f489db
Vivek Bhat <vivek.bhat@hughes.com>
2024-10-14 09:05:51 -0400

    Adding aws helpers and work stuff

--- Unix/OperatingSystems/Linux/work/backup_owaiops.sh
+++ Unix/OperatingSystems/Linux/work/backup_owaiops.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
 # Function to commit and push changes with today's date and time as the commit message
-backup_owaiops() {
-    # Check if there are changes to commit
-    if git diff-index --quiet HEAD --; then
-        echo "No changes to commit."
-    else
-        # Get today's date and time in YYYY-MM-DD HH:MM:SS format
-        commit_message=$(date +"%Y-%m-%d %H:%M:%S")
+function backup_owaiops() {
+    # Change to the repository directory
+    (
+        cd "$REPO_DIR" || {
+            echo "Repository directory not found: $REPO_DIR"
+            exit 1
+        }
 
-        # Check if the remote is set
-        if ! git remote get-url origin >/dev/null 2>&1; then
-            echo "Remote 'origin' is not set. Setting remote to https://github.com/VivekBhat/pvt_owaiops.git"
-            git remote add origin https://github.com/VivekBhat/pvt_owaiops.git
+        # Check if there are changes to commit
+        if git diff-index --quiet HEAD --; then
+            echo "No changes to commit."
+        else
+            # Get today's date and time in YYYY-MM-DD HH:MM:SS format
+            commit_message=$(date +"%Y-%m-%d %H:%M:%S")
+            trace "commit message=$commit_message"
+
+            # Check if the remote is set
+            if ! git remote get-url origin >/dev/null 2>&1; then
+                echo "Remote 'origin' is not set. Setting remote to https://github.com/VivekBhat/pvt_owaiops.git"
+                git remote add origin https://github.com/VivekBhat/pvt_owaiops.git
+            fi
+
+            # Stage all changes
+            git add .
+
+            # Commit with today's date and time as the message
+            git commit -m "$commit_message"
+
+            # Push to the current branch
+            git push origin $(git branch --show-current)
+
+            # Confirmation message
+            echo "Changes committed and pushed with message: $commit_message"
         fi
+    )
+}
 
-        # Stage all changes
-        git add .
+function zip_folder() {
+    local TARGET_DIR="/home/vivekbhat/Projects/owaiops/fault-localization-service-terraform"
+    local ZIP_FILE="/home/vivekbhat/Projects/private/owaiops_backup/backup.zip"
+    local EXCLUDES=("*.git/*" "*.terraform/*")
 
-        # Commit with today's date and time as the message
-        git commit -m "$commit_message"
+    # Create the zip file excluding the specified directories
+    zip -r "$ZIP_FILE" "$TARGET_DIR" -x "${EXCLUDES[@]}"
 
-        # Push to the current branch
-        git push origin $(git branch --show-current)
-
-        # Confirmation message
-        echo "Changes committed and pushed with message: $commit_message"
-    fi
+    echo "Zipping complete: $ZIP_FILE"
 }
