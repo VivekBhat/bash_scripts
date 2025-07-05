238c637ebfc74fe438a72bbf6750177fd340141e
Vivek Bhat <vivek.bhat@hughes.com>
2025-07-05 10:22:22 -0400

    Updating refresh maven token method

--- Linux/work/aws_helpers.sh
+++ Linux/work/aws_helpers.sh
@@ -1,12 +1,44 @@
 #!/bin/bash
+function refresh-ecr-login() {
+    aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 181148949657.dkr.ecr.eu-west-2.amazonaws.com
+}
+
 function refresh-maven-token() {
-    export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token \
+    # Define the file where the token and expiration timestamp will be stored
+    TOKEN_FILE=~/.maven_token_info
+
+    # Check if the token file exists and read the expiration time
+    if [[ -f $TOKEN_FILE ]]; then
+        # Read the expiration time from the file
+        TOKEN_EXPIRATION=$(cat $TOKEN_FILE | grep 'expiration' | cut -d'=' -f2)
+        CURRENT_TIME=$(date +%s)
+
+        # Check if the current time is greater than the expiration time
+        if [[ $CURRENT_TIME -lt $TOKEN_EXPIRATION ]]; then
+            echo "No need to update CodeArtifact token as it is still valid."
+            return 0
+        fi
+    fi
+
+    # If the token is expired or the file does not exist, refresh the token
+    echo "Refreshing CodeArtifact token..."
+    CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token \
         --domain hughes \
         --domain-owner 181148949657 \
         --region eu-west-2 \
         --query authorizationToken \
         --output text)
+
+    # Store the token and the new expiration time (current time + 12 hours)
+    EXPIRATION_TIMESTAMP=$(($(date +%s) + 43200))
+    echo "authorizationToken=$CODEARTIFACT_AUTH_TOKEN" >$TOKEN_FILE
+    echo "expiration=$EXPIRATION_TIMESTAMP" >>$TOKEN_FILE
+
+    # Export the token for the current session
+    export CODEARTIFACT_AUTH_TOKEN
 }
+
+refresh-maven-token &>/dev/null
 
 alias aws-kubectl-update='aws eks update-kubeconfig --region eu-west-2 --name insightalytics'
 
@@ -26,7 +58,3 @@ function connect-ec2() {
 }
 
 export AWS_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
-
-function refresh-ecr-login() {
-    aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 181148949657.dkr.ecr.eu-west-2.amazonaws.com
-}
