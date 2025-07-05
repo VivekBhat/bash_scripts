5fc5e8c392d5565665e83ac5e91e7e2d25dd15ce
Vivek Bhat <bhatvivek93@gmail.com>
2025-07-05 00:27:06 -0400

    Updated mbp15

--- Unix/computers/MBP15/installer.sh
+++ Unix/computers/MBP15/installer.sh
@@ -6,7 +6,7 @@ source $UNIX_SCRIPT_DIR/common_scripts/bash_lib/common.sh
 check_and_install_zsh
 
 # Add to ZSH Profile
-add_profile_line ~/.zshrc $SCRIPT_DIR/zsh_profile
+add_profile_line ~/.zshrc $SCRIPT_DIR/profile
 
 # Add to Bash Profile
-add_profile_line ~/.bashrc $SCRIPT_DIR/bash_profile
+add_profile_line ~/.bashrc $SCRIPT_DIR/profile
