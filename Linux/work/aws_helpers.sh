aebc1b6607c50b79f3165d90b8fba02941a8d1c1
Vivek Bhat <vivek.bhat@hughes.com>
2025-07-05 10:22:22 -0400

    Added AWS helper methods needed at work

--- Linux/work/aws_helpers.sh
+++ Linux/work/aws_helpers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function refresh_maven_token() {
+function refresh-maven-token() {
     export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token \
         --domain hughes \
         --domain-owner 181148949657 \
@@ -10,20 +10,23 @@ function refresh_maven_token() {
 
 alias aws-kubectl-update='aws eks update-kubeconfig --region eu-west-2 --name insightalytics'
 
-function connect_ds() {
-    connect_ec2 "ia-deployment-server"
+function connect-ds() {
+    connect-ec2 "ia-deployment-server"
 }
 
-function connect_msk() {
-    connect_ec2 "MSK-CLIENT"
+function connect-msk() {
+    connect-ec2 "MSK-CLIENT"
 }
 
-function connect_ec2() {
+function connect-ec2() {
     local instance_name=$1
     local instance_id=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$instance_name" | jq -r '.Reservations[].Instances[] | select(.State.Name == "running") | .InstanceId')
     echo "connecting to instance_name=$instance_name, instance_id=$instance_id"
     aws ssm start-session --target $instance_id
 }
 
-refresh_maven_token
 export AWS_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"
+
+function refresh-ecr-login() {
+    aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 181148949657.dkr.ecr.eu-west-2.amazonaws.com
+}
