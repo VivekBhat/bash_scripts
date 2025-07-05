62718efa323f726f16804dffb963e5a6def80ca9
Vivek Bhat <vivekbhat@Viveks-MacBook-Pro.local>
2025-07-05 10:22:22 -0400

    Added personal macbook setting

---
+++ Mac_Personal/zsh_script.sh
@@ -0,0 +1,63 @@
+#--------------------------------------
+# Personalized additions
+#--------------------------------------
+
+projectsFolder=$HOME/Projects
+scriptsFolder=$projectsFolder/bash_scripts/Mac_Personal
+
+# cd & ls in one
+function chpwd() {
+    emulate -L zsh
+    ls
+}
+
+runCleanupScripts(){
+	mkdir -p $projectsFolder
+	$scriptsFolder/cleanup $HOME
+}
+
+#--------------------------------------
+# Function to move to desktop quickly
+#--------------------------------------
+
+desktop () {
+	cd $HOME/Desktop
+	echo "Moved to Desktop :) "
+}
+
+openProjects(){
+	cd $projectsFolder/$1
+	echo "Moved to $1 folder :) "
+	# No space after $1 is intentional 
+}
+#--------------------------------------
+# Function to edit zshrc
+#--------------------------------------
+
+prof(){
+	echo "Opening Visual Studio Code to edit the bash profile"
+	cd $scriptsFolder
+	code . 1>/dev/null
+}
+
+#--------------------------------------
+# Function to Source bash profile
+#--------------------------------------
+src(){
+	source ~/.zshrc
+	echo "Bash Profile Version: $bashProfileVersion"
+}
+
+#--------------------------------------
+# Function to write notes
+#--------------------------------------
+
+notes(){
+	echo "Let's take notes"
+	cd $projectsFolder/Notes
+	code . 1>/dev/null
+}
+
+runCleanupScripts
+
+echo $PATH
