7713249279f0d4e3ce87afa0d4ac069884bc7d7d
Vivek Bhat <bhatvivek93@gmail.com>
2025-07-09 19:11:46 -0400

    added gl_helper

--- Unix/common_scripts/bash_lib/personal_helpers.sh
+++ Unix/common_scripts/bash_lib/personal_helpers.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 mount_g(){
-    echo "Mounting G: drive to /mnt/g"
+    echo "Mounting 'G: drive' to /mnt/g"
     mkdir -p /mnt/g
     sudo mount -t drvfs G: /mnt/g
 }
 
 mount_gl(){
     mount_g
-    # create link to this folder from Drive to WSL: /mnt/g/My Drive/Documents/_Vivek/School/05_MIT_ADSP/Resources
-    mkdir -p /mnt/g/My\ Drive/Documents/_Vivek/School/05_MIT_ADSP/Resources
-    ln -s /mnt/g/My\ Drive/Documents/_Vivek/School/05_MIT_ADSP/Resources /home/vivekbhat/Projects/private/GL/GoogleDriveResources
-    code /home/vivekbhat/Projects/private/GL
+    GL_FOLDER="/home/vivekbhat/Projects/private/GL"
+    TARGET="$GL_FOLDER/GoogleDriveResources"
+    SOURCE="/mnt/g/My Drive/Documents/_Vivek/School/05_MIT_ADSP/Resources"
+
+    # Check if the target path does not exist
+    if [ ! -e "$TARGET" ]; then
+        ln -s "$SOURCE" "$TARGET"
+        echo "Symbolic link created: $TARGET → $SOURCE"
+    else
+        echo "Path already exists: $TARGET"
+    fi
+    echo "Opening $GL_FOLDER in VSCode"
+    code $GL_FOLDER
 }
