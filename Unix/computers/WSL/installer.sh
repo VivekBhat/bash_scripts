c56dbb60047522f6d51781ead898e0d2d7e5eed6
Vivek Bhat <vivek.bhat@hughes.com>
2025-07-18 11:48:56 -0400

    Updated script dir for wsl work

--- Unix/computers/WSL/installer.sh
+++ Unix/computers/WSL/installer.sh
@@ -3,9 +3,6 @@
 # First run the Unix/installer.sh to install in WSL #
 #####################################################
 
-WSL_SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-
-# SCRIPT_DIR=$(dirname "$0")
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source $UNIX_SCRIPT_DIR/common_scripts/bash_lib/common.sh
@@ -14,4 +11,4 @@ check_oh_my_bash
 check_github_desktop_automatic_update
 
 # Add Bash Profile line
-add_profile_line ~/.bashrc $WSL_SCRIPT_DIR/bash_profile
+add_profile_line ~/.bashrc $SCRIPT_DIR/bash_profile
