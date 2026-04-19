4a66dbc82ee8dbc8531e3bd258711f507f73e8b8
Vivek Bhat <bhatvivek93@gmail.com>
2026-04-19 16:18:37 -0400

    added ubuntu

---
+++ Unix/computers/HP_Ubuntu/installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#####################################################
+# First run the Unix/installer.sh to install in WSL #
+#####################################################
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+source $UNIX_SCRIPT_DIR/common_scripts/bash_lib/common.sh
+
+check_oh_my_bash
+check_github_desktop_automatic_update
+
+# Add Bash Profile line
+add_profile_line ~/.bashrc $SCRIPT_DIR/bash_profile
